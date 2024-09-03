86a441a688e89eff46008856ee2b88ed176d283f
winnie houng <kiots@LAPTOP-4E9ES74J>
2024-09-03 17:28:23 -0500

    changed cosB, sinB to single variable cosBsinB to prevent 2 getUniformLocation calls

--- canvas/canvas2.js
+++ canvas/canvas2.js
@@ -8,7 +8,6 @@
  * y'=rsin(A+B)=r(cosAsinB+cosBsinA)=xsinB+ycosB
  * z'= z
  */
-
 main2(); 
 
 function main2() {
@@ -17,7 +16,7 @@ function main2() {
     const context = getContext(canvas);
 
     // shader programs
-    const vShaderSrc = 'attribute vec4 vPosition; uniform float cosB, sinB; void main(void){gl_Position.x=vPosition.x*cosB-vPosition.y*sinB; gl_Position.y=vPosition.x*sinB+vPosition.y*cosB; gl_Position.z=vPosition.z; gl_Position.w=1.0;}';
+    const vShaderSrc = 'attribute vec4 vPosition; uniform vec2 cosBsinB; void main(void){gl_Position.x=vPosition.x*cosBsinB.x-vPosition.y*cosBsinB.y; gl_Position.y=vPosition.x*cosBsinB.y+vPosition.y*cosBsinB.x; gl_Position.z=vPosition.z; gl_Position.w=1.0;}';
 
     const fShaderSrc = 'precision mediump float; uniform vec4 fColor; void main(void){gl_FragColor=fColor;}';
 
@@ -47,16 +46,16 @@ function render(context) {
     context.uniform4f(fLocation, 1.0, 0.5, 0.31, 1.0);
 
     // rotation data
-    const angleB=90.0;
+    const angleB=-90.0;
     const radianB=Math.PI*angleB/180.0;
     const cosB=Math.cos(radianB);
     const sinB=Math.sin(radianB);
+    const cosBsinB=new Float32Array([cosB, sinB]);
 
     // assign vertex rotations
-    const cosBLocation=context.getUniformLocation(context.shaderProgram, 'cosB');
-    const sinBLocation=context.getUniformLocation(context.shaderProgram, 'sinB');
-    context.uniform1f(cosBLocation, cosB);
-    context.uniform1f(sinBLocation, sinB);
+    const cosBsinBLocation=context.getUniformLocation(context.shaderProgram, 'cosBsinB');
+
+    context.uniform2f(cosBsinBLocation, cosBsinB[0], cosBsinB[1]);
 
     clearCanvas(context, [0.799, 0.799, 0.799, 1.0]);
     context.drawArrays(context.TRIANGLES, 0, data.length / nComponents);
