3a25ac5957542ed3efb0455dbd158b2299d476e2
winnie houng <kiots@LAPTOP-4E9ES74J>
2024-09-03 17:14:47 -0500

    manual rotation using trig func add theorem

--- canvas/canvas2.js
+++ canvas/canvas2.js
@@ -1,14 +1,15 @@
 /**
  * 'v' prefix for vertex
  * 'f' prefix for fragment
+ * 
+ * rotation info
+ * x=rcosA, y=rsinA: rotate by B, about z-axis
+ * x'=rcos(A+B)=r(cosAcosB-sinAsinB)=xcosB-ysinB
+ * y'=rsin(A+B)=r(cosAsinB+cosBsinA)=xsinB+ycosB
+ * z'= z
  */
 
-// translations
-const Tx = 0.5, Ty = 0.5, Tz = 0.0;
-
 main2(); 
-
-
 
 function main2() {
 
@@ -16,7 +17,7 @@ function main2() {
     const context = getContext(canvas);
 
     // shader programs
-    const vShaderSrc = 'attribute vec4 vPosition; uniform vec4 vTranslation;void main(void){gl_Position=vPosition+vTranslation;}';
+    const vShaderSrc = 'attribute vec4 vPosition; uniform float cosB, sinB; void main(void){gl_Position.x=vPosition.x*cosB-vPosition.y*sinB; gl_Position.y=vPosition.x*sinB+vPosition.y*cosB; gl_Position.z=vPosition.z; gl_Position.w=1.0;}';
 
     const fShaderSrc = 'precision mediump float; uniform vec4 fColor; void main(void){gl_FragColor=fColor;}';
 
@@ -45,9 +46,17 @@ function render(context) {
     const fLocation = context.getUniformLocation(context.shaderProgram, 'fColor');
     context.uniform4f(fLocation, 1.0, 0.5, 0.31, 1.0);
 
-    // assign vertex translations
-    const vTranslationLocation=context.getUniformLocation(context.shaderProgram, 'vTranslation');
-    context.uniform4f(vTranslationLocation, Tx, Ty, Tz, 0.0);
+    // rotation data
+    const angleB=90.0;
+    const radianB=Math.PI*angleB/180.0;
+    const cosB=Math.cos(radianB);
+    const sinB=Math.sin(radianB);
+
+    // assign vertex rotations
+    const cosBLocation=context.getUniformLocation(context.shaderProgram, 'cosB');
+    const sinBLocation=context.getUniformLocation(context.shaderProgram, 'sinB');
+    context.uniform1f(cosBLocation, cosB);
+    context.uniform1f(sinBLocation, sinB);
 
     clearCanvas(context, [0.799, 0.799, 0.799, 1.0]);
     context.drawArrays(context.TRIANGLES, 0, data.length / nComponents);
